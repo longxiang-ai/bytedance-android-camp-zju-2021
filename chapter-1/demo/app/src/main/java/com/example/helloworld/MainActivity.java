bf7c01c5c732482b50ccd1470b00498461eda0d9
zhouyihang <zhouyihang.cmmsz@bytedance.com>
2021-07-12 08:08:13 +0800

    添加打印日志

--- chapter-1/demo/app/src/main/java/com/example/helloworld/MainActivity.java
+++ chapter-1/demo/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv1 = findViewById(R.id.tv_1);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "abc");
+            }
+        });
     }
 }
